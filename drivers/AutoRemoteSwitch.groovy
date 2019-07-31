c593771617ccf408f6c2105d15a8ee030ccedd9f
Josh Lyon <josh@boshdirect.com>
2019-07-31 10:01:30 -0500

    Formatting tweaks

--- drivers/AutoRemoteSwitch.groovy
+++ drivers/AutoRemoteSwitch.groovy
@@ -2,15 +2,15 @@
 metadata {
 	definition (name: "AutoRemote Wifi Device", namespace: "joshualyon/testing", author: "josh") {
 		capability "Sensor"
-        capability "Switch"
+		capability "Switch"
 		command "statusOn"
 		command "statusOff"
-        command "sendCommand", ["string"]
-        command "sendCommandMessage", ["string", "string"]
+		command "sendCommand", ["string"]
+		command "sendCommandMessage", ["string", "string"]
         
         preferences {
-		    input(name: "deviceIP", type: "string", title:"AutoRemote Device IP Address", description: "", required: true, displayDuringSetup: true)
-            input(name: "customCommand", type: "string", title:"Default Command to Send", description: "ex: {command}=:=on", displayDuringSetup: true)
+		input(name: "deviceIP", type: "string", title:"AutoRemote Device IP Address", description: "", required: true, displayDuringSetup: true)
+		input(name: "customCommand", type: "string", title:"Default Command to Send", description: "ex: {command}=:=on", displayDuringSetup: true)
 	    }
 	}
 }
@@ -18,7 +18,7 @@ metadata {
 def installed() {
 	log.trace "Executing 'installed'"
 	initialize()
-    off()
+	off()
 }
 
 def updated() {
@@ -31,62 +31,62 @@ private initialize() {
 }
 
 def statusOn(){
-    log.debug "Updating the status to 'on' without sending network command"
-    on(false)
+	log.debug "Updating the status to 'on' without sending network command"
+	on(false)
 }
 
 def statusOff(){
-    log.debug "Updating the status to 'off' without sending network command"
-    off(false)
+	log.debug "Updating the status to 'off' without sending network command"
+	off(false)
 }
 
 def on(isRemoteCommand=true) {
-    log.trace "Executing 'on'"
-    sendEvent(name: "switch", value: "on", isStateChange: true)
-    if(isRemoteCommand)
-        sendCustomCommand("on")
+	log.trace "Executing 'on'"
+	sendEvent(name: "switch", value: "on", isStateChange: true)
+	if(isRemoteCommand)
+		sendCustomCommand("on")
 }
 
 def off(isRemoteCommand=true) {
-    log.trace "Executing 'off'"
-    sendEvent(name: "switch", value: "off", isStateChange: true)
-    if(isRemoteCommand)
-        sendCustomCommand("off")
+	log.trace "Executing 'off'"
+	sendEvent(name: "switch", value: "off", isStateChange: true)
+	if(isRemoteCommand)
+		sendCustomCommand("off")
 }
 
 def sendCommand(command){
-    log.debug "Sending command: ${command}"
-    sendARCommand(command)
+	log.debug "Sending command: ${command}"
+	sendARCommand(command)
 }
 
 def sendCommandMessage(command, message){
-    log.debug "Sending command with message: ${command}=:=${message}"
-    sendARCommand("${command}=:=${message}")
+	log.debug "Sending command with message: ${command}=:=${message}"
+	sendARCommand("${command}=:=${message}")
 }
 
 def sendCustomCommand(command){
-    log.debug "Sending default custom command: ${customCommand}=:=${command}"
-    sendARCommand("${customCommand}=:=${command}")
+	log.debug "Sending default custom command: ${customCommand}=:=${command}"
+	sendARCommand("${customCommand}=:=${command}")
 }
 
 def sendARCommand(command){
 	def path = varCommand 
 	def body = "" 
 	def headers = [:] 
-    headers.put("HOST", "${deviceIP}:1817")
+	headers.put("HOST", "${deviceIP}:1817")
 
 	try {
 		def hubAction = new hubitat.device.HubAction(
 			method: "GET",
-            path: "/?message=${command}",
+			path: "/?message=${command}",
 			body: body,
 			headers: headers
-        )
+        	)
 		log.debug hubAction
 		return hubAction
 	}
-	catch (Exception e) {
-        log.debug "sendARCommand hit exception ${e} on ${hubAction}"
+	catch (Exception err) {
+        	log.debug "sendARCommand error ${err} when sending HubAction ${hubAction}"
 	}  
 }
 
